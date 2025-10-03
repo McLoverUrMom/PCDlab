d993158e9bdcf30808cdbf8b3619f73d830efcd8
Raval <dsprincan04@gmail.com>
2025-10-03 20:25:27 +0300

    исправлены ошибки

--- lab1/Main.java
+++ lab1/Main.java
@@ -1,16 +1,18 @@
 import java.util.Random;
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+
+        Random random = new Random;
+        int mas[] = new mas[100]; // массив случайных чисел
+        System.out.println("Создан массив с генерацией случайных чисел")
+
+        for (int =0;i < 100;i++){
+            mas[i] = random.nextInt(100); // заполнение массива
+        }
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Случайно сгенерированные числа:" + mas[i]);
+        }
+    }
 }
-
-    Random random = new Random;
-    int mas[] = new mas[100]; // массив случайных чисел
-    System.out.println("Создан массив с генерацией случайных чисел")
-
-    for(int = 0; i < 100; i++){
-        mas[i] = random.nextInt(100); // заполнение массива
-        }
-
-    for( int i = 0; i < 100; i++){
-        System.out.println("Случайно сгенерированные числа:" +mas[i]);
-        }
