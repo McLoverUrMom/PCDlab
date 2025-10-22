6de483a0822c8003252728b71a9ead9886ab01df
Raval <dsprincan04@gmail.com>
2025-10-22 21:28:11 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,14 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Choose your fighter: 1.Sprincean, 2.Lutschii");
+        int var = scan.nextInt();
 
-        Random random = new Random();
-        int mas[] = new int[100]; // массив случайных чисел
-        System.out.println("Создан массив с генерацией случайных чисел");
-
-        for (int i = 0; i < 100; i++){
-            mas[i] = random.nextInt(100); // заполнение массива
+        switch(var){
+            case (1): sprincean.chinese_start();
+            case (2): Lutschii.medvedi_start();
         }
-
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Случайно сгенерированные числа:" + mas[i]);
-        }
-
-        int choice = sc.nextInt();
-        switch(choice){
-                case 1:
-                   Thread thread1 = new Thread(new ThreadS1(mas));
-                   Thread thread2 = new Thread(new ThreadS2(mas));
-                    thread1.start();
-                    thread2.start();
-
-                    break;
-                case 2:
-                    Thread thread3 = new Thread(new ThreadL1(mas));
-                    thread3.start();
-                    Thread thread4 = new Thread(new ThreadL2(mas));
-                    thread4.start();
-
-                    break;
-                default:
-                    System.out.println("Нет такого варианта");
-        }
-
-        String message = " Работу выполнили Лучицкий и Спринчан";
-        for (char ch : message.toCharArray()) {
-            System.out.print(ch);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.out.println("\nПоток был прерван");
-                break;
-            }
-        }
-        System.out.println();
-
     }
 }
